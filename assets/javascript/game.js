ed70b5d33b3d70af36a46f9ea7f2f545fd7ff20c
Samuel Jeon <samueljeon@Samuels-Air.attlocal.net>
2017-04-29 02:38:11 -0500

    most of the javascript done, game functions but need to make it look pretty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,163 @@
-var wordOptions = ["direwolf", "baratheon", "dragon", "stark", "targareon", "lanister",
+
+//Computer chooses random word from array and displays the correct number of blanks for that word//
+var wordOptions = ["direwolf", "baratheon", "dragon", "stark", "targaryen", "lannister",
 					"wildling"];
 
 
-	var wordChosen = wordOptions[Math.floor(Math.random()*wordOptions.length)];
+
+	var winCounter = 0;
+
+	var winCountDiv = document.getElementById("wins");
+	winCountDiv.innerHTML = winCounter;
+
+
+function startGame() {
+
+//Counter for number of guesses remaining//
+	var counter = 10;
+
+	var counterDiv = document.getElementById("guessNumber");
+	counterDiv.innerHTML = counter;
+
+//reset the blanks for letters//
+	var blankDiv = document.getElementById("blanks");
+
+	blankDiv.innerHTML = "";
+//reset the guessed letters array//
+	var guessDiv = document.getElementById("guessed");
+	guessDiv.innerHTML = "";
+
+
+//computer randomly chooses word from wordOptions array//
+var wordChosen = wordOptions[Math.floor(Math.random()*wordOptions.length)];
 
 
 	alert("You've chosen " + wordChosen);
 
-	var blanks = wordChosen.replace(/./g, " _ ");
+for (var a = 0; a < wordChosen.length; a++) {
 
-	console.log(blanks);
-
-	//var split = wordChosen.split("");
-
-	//alert(split);
-
-//var replaceLetters = function(x) {
-	//for (var i = 0; i < wordChosen.length; i++) {
-		//var test = wordChosen.replace(wordChosen[i],"_");
-
-		//console.log(test);
-	//}
-
-		//alert(replaceLetters);
-
-	//var test = split.fill(" _ ");
-		//alert(test);
 	
-
-
-//replaceLetters();
-
-	//var blanks = wordChosen.replace(wordChosen, 1*wordChosen.length);
-
-	//alert(blanks);
-
-	var currentWord = "<p>Current Word:</p>" +
-					"<p>" + blanks + "</p>";
-
-	document.querySelector('#currentWord').innerHTML = currentWord;
-
-
-
-
-
-
-document.onkeyup = function(event) {
-
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	alert(userGuess);
+	blankDiv.innerHTML = blankDiv.innerHTML + "_";
 }
+
+
+
+
+
+
+//User inputs a letter to try to guess the word
+//computer takes the input and checks each letter of chosen word
+//if the gueesed letter matches the letter of chosen word at that position, replace the blank at tht position with the gueesed letter
+// if the guessed letter does not match, leave the blank
+
+//array that keeps track of all the guessed letters//
+var allGuessArr = [];
+
+
+
+
+
+//User guesses letter with keyboard//
+document.onkeyup = function(event) {
+	
+//Counter goes down by 1 each time a guess is made//
+	counter--;
+	counterDiv.innerHTML = counter;
+
+
+
+
+//keeps the actual guess of the user//
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+//Check if the letter has already been guessed//
+	var duplicateCheck = allGuessArr.includes(userGuess);
+	if (duplicateCheck === true) {
+				return;
+			}
+
+//array that keeps track of the possible indexes that the guessed letter is on the chosen word//
+	var possibleIndex = [];
+
+//Computer checks if the guessed letter is in the chosen word and sends the position to array//
+	for (var b = 0; b < wordChosen.length; b++) {
+		if (userGuess == wordChosen[b]) {
+			possibleIndex.push(b);
+			}
+		else {
+			possibleIndex.push("");
+			}	
+	}
+
+
+//if the guessed letter is in the word, computer replaces the correct blank with the letter//
+		var newBlanks = "";	
+
+		for (var d = 0; d < blankDiv.innerHTML.length; d++) {
+
+			
+
+			
+			
+			if (blankDiv.innerHTML[d] !== "_") {
+				newBlanks += blankDiv.innerHTML[d];
+			}
+			
+			if (d === possibleIndex[d]) {
+
+				newBlanks += userGuess;
+				
+				console.log(newBlanks);
+			}
+
+			if (blankDiv.innerHTML[d] == "_" && d !== possibleIndex[d]) {
+
+				 newBlanks += "_";
+			}	
+
+		}
+
+		blankDiv.innerHTML = newBlanks;
+		allGuessArr.push(userGuess);
+
+		if (blankDiv.innerHTML === wordChosen) {
+
+			winCounter++;
+			winCountDiv.innerHTML = winCounter;
+			//wordOptions.splice(wordOptions.indexof(wordChosen), 1);
+			alert("You guessed " + wordChosen + " correctly! Click the Start button to play again!");
+
+		}
+
+
+
+		
+
+		
+
+
+	//Place wrongly guessed letters in guessDiv//
+		var guessArr = [];
+
+		var letterCheck = wordChosen.includes(userGuess);
+
+		if (letterCheck === false) {
+
+		guessArr.push(" " + userGuess + " ");
+
+		}
+		//var guessDiv = document.getElementById("guessed");
+		guessDiv.innerHTML = guessDiv.innerHTML + guessArr;
+
+
+	
+	
+	
+}
+
+
+}
+
+
+
+
