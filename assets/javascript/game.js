4d04e71f7f4db3bc875ccd75b79f3d074fbcb41e
Samuel Jeon <samueljeon@Samuels-Air.attlocal.net>
2017-04-29 22:01:05 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 
+
 //Computer chooses random word from array and displays the correct number of blanks for that word//
-var wordOptions = ["direwolf", "baratheon", "dragon", "stark", "targaryen", "lannister",
+var wordOptions = ["winter", "baratheon", "dragon", "stark", "targaryen", "lannister",
 					"wildling"];
 
 
@@ -13,8 +14,9 @@ var wordOptions = ["direwolf", "baratheon", "dragon", "stark", "targaryen", "lan
 
 function startGame() {
 
+
 //Counter for number of guesses remaining//
-	var counter = 10;
+	var counter = 15;
 
 	var counterDiv = document.getElementById("guessNumber");
 	counterDiv.innerHTML = counter;
@@ -29,65 +31,44 @@ function startGame() {
 
 
 //computer randomly chooses word from wordOptions array//
-var wordChosen = wordOptions[Math.floor(Math.random()*wordOptions.length)];
+	var wordChosen = wordOptions[Math.floor(Math.random()*wordOptions.length)];
 
 
-	alert("You've chosen " + wordChosen);
+	//alert("You've chosen " + wordChosen);
 
-for (var a = 0; a < wordChosen.length; a++) {
+	for (var a = 0; a < wordChosen.length; a++) {
 
-	
-	blankDiv.innerHTML = blankDiv.innerHTML + "_";
-}
-
-
-
-
-
-
-//User inputs a letter to try to guess the word
-//computer takes the input and checks each letter of chosen word
-//if the gueesed letter matches the letter of chosen word at that position, replace the blank at tht position with the gueesed letter
-// if the guessed letter does not match, leave the blank
+		blankDiv.innerHTML = blankDiv.innerHTML + "_";
+		}
 
 //array that keeps track of all the guessed letters//
-var allGuessArr = [];
-
-
-
-
+	var allGuessArr = [];
 
 //User guesses letter with keyboard//
-document.onkeyup = function(event) {
+	document.onkeyup = function(event) {
 	
-//Counter goes down by 1 each time a guess is made//
-	counter--;
-	counterDiv.innerHTML = counter;
-
-
-
 
 //keeps the actual guess of the user//
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 //Check if the letter has already been guessed//
-	var duplicateCheck = allGuessArr.includes(userGuess);
-	if (duplicateCheck === true) {
+		var duplicateCheck = allGuessArr.includes(userGuess);
+		if (duplicateCheck === true) {
 				return;
 			}
 
 //array that keeps track of the possible indexes that the guessed letter is on the chosen word//
-	var possibleIndex = [];
+		var possibleIndex = [];
 
 //Computer checks if the guessed letter is in the chosen word and sends the position to array//
-	for (var b = 0; b < wordChosen.length; b++) {
-		if (userGuess == wordChosen[b]) {
-			possibleIndex.push(b);
+		for (var b = 0; b < wordChosen.length; b++) {
+			if (userGuess == wordChosen[b]) {
+				possibleIndex.push(b);
+				}
+			else {
+				possibleIndex.push("");
+				}	
 			}
-		else {
-			possibleIndex.push("");
-			}	
-	}
 
 
 //if the guessed letter is in the word, computer replaces the correct blank with the letter//
@@ -95,10 +76,6 @@ document.onkeyup = function(event) {
 
 		for (var d = 0; d < blankDiv.innerHTML.length; d++) {
 
-			
-
-			
-			
 			if (blankDiv.innerHTML[d] !== "_") {
 				newBlanks += blankDiv.innerHTML[d];
 			}
@@ -120,21 +97,28 @@ document.onkeyup = function(event) {
 		blankDiv.innerHTML = newBlanks;
 		allGuessArr.push(userGuess);
 
+		//Counter goes down by 1 each time a guess is made//
+		counter--;
+		counterDiv.innerHTML = counter;
+
 		if (blankDiv.innerHTML === wordChosen) {
 
 			winCounter++;
 			winCountDiv.innerHTML = winCounter;
-			//wordOptions.splice(wordOptions.indexof(wordChosen), 1);
-			alert("You guessed " + wordChosen + " correctly! Click the Start button to play again!");
+			
+			alert("You guessed  " + wordChosen + " correctly! Try to guess the next word!")
+
+			startGame();
 
 		}
 
+		if (counter === 0 && blankDiv.innerHTML !== wordChosen) {
 
+			alert("You lost! Try again!");
 
-		
+			startGame();
 
-		
-
+		}
 
 	//Place wrongly guessed letters in guessDiv//
 		var guessArr = [];
@@ -149,14 +133,10 @@ document.onkeyup = function(event) {
 		//var guessDiv = document.getElementById("guessed");
 		guessDiv.innerHTML = guessDiv.innerHTML + guessArr;
 
-
-	
-	
-	
+	}
 }
 
-
-}
+startGame();
 
 
 
